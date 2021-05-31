7c1f3c6f33b2ed6b5bb23e89ec670b1d9ae2294d
pvanhungit <pvanhungit@gmail.com>
2021-05-31 20:13:45 +0700

    rfr

--- src/App2.java
+++ src/App2.java
@@ -4,8 +4,8 @@ public class App2 {
     public static void main(String[] args) throws Exception {
         App2 app = new App2();
 
-        int[] nums = new int[] { 2, 2, 2, 2, 2 };
-        //int[] nums = new int[]{1,3,5,4,7,8};
+        //int[] nums = new int[] { 1,2,3,4,6,8,3,4,5 };
+        int[] nums = new int[]{1,3,5,4,7,8};
         //int[] nums = new int[]{3,1,2};
 
         int rs = app.findNumberOfLIS(nums);
@@ -21,8 +21,7 @@ public class App2 {
         for(int i = 0 ; i < nums.length; i++) {
             int value = nums[i];
 
-            lens[i]++;
-            cnt[i]++;
+            lens[i] = cnt[i] = 1;
             for(int k = 0; k < i; k++) {
                 int valueK = nums[k], lenK = lens[k];
                 if(valueK < value ) {

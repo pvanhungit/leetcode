5e339dee287ec1f43f5024d765810c1bb890e1cb
pvanhungit <pvanhungit@gmail.com>
2021-11-21 10:57:26 +0700

    interver leaving string

--- src/App5.java
+++ src/App5.java
@@ -3,14 +3,33 @@ public class App5 {
 
     public static void main(String[] args) throws Exception {
         App5 app = new App5();
-        boolean rs = app.longestPalindrome("aaccb", "aaccd", "aaaaccccdb");
-        System.out.println(rs);
+        boolean rs = false;
+
+        // rs = app.isInterleave("aabcc", "dbbca", "aadbbcbcac");
+        // System.out.println("case 1 : " + rs);
+
+        // rs = app.isInterleave("aabcc", "dbbca", "aadbbbaccc");
+        // System.out.println("case 2 : " + rs);
+        
+        // rs = app.isInterleave("", "", "");
+        // System.out.println("case 3 : " + rs);
+
+        // rs = app.isInterleave("aabd", "abdc", "aabdabcd");
+        // System.out.println("case 4 : " + rs);
+
+        rs = app.isInterleave("bbbbbabbbbabaababaaaabbababbaaabbabbaaabaaaaababbbababbbbbabbbbababbabaabababbbaabababababbbaaababaa",
+         "babaaaabbababbbabbbbaabaabbaabbbbaabaaabaababaaaabaaabbaaabaaaabaabaabbbbbbbbbbbabaaabbababbabbabaab", 
+         "babbbabbbaaabbababbbbababaabbabaabaaabbbbabbbaaabbbaaaaabbbbaabbaaabababbaaaaaabababbababaababbababbbababbbbaaaabaabbabbaaaaabbabbaaaabbbaabaaabaababaababbaaabbbbbabbbbaabbabaabbbbabaaabbababbabbabbab");
+        System.out.println("case 4 : " + rs);
     }
 
     public boolean isInterleave(String s1, String s2, String s3) {
-        boolean rs = true;
+        if( (s1 + s2).equals(s3) || (s2 + s1).equals(s3)) return true;
 
-        return rs;
+        boolean rs1 = s1.length() >0 && s3.length() > 0 && s1.charAt(0) == s3.charAt(0);
+        boolean rs2 = s2.length() >0 && s3.length() > 0 && s2.charAt(0) == s3.charAt(0);
+        return rs1 &&  isInterleave(s2, s1.substring(1, s1.length()), s3.substring(1, s3.length())) ||
+        rs2 && isInterleave(s2.substring(1, s2.length()) , s1 , s3.substring(1, s3.length()));
 
     }
 }
